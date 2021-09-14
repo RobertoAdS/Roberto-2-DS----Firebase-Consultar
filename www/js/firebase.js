830aaf226975c1856bd7287d5d5fea127256412d
RobertoAdS <90658865+RobertoAdS@users.noreply.github.com>
2021-09-13 21:01:10 -0300

    Add files via upload

---
+++ www/js/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCfS-87Fjqe457p6-8uXodIoUze8lciEpg",
+    authDomain: "cordova-agendamentos.firebaseapp.com",
+    projectId: "cordova-agendamentos",
+    storageBucket: "cordova-agendamentos.appspot.com",
+    messagingSenderId: "439206951756",
+    appId: "1:439206951756:web:3454369e979271370a2de9",
+    measurementId: "G-E98RDL2CW8"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
